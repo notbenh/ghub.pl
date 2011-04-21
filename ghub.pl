028ac29518cf265998bce32ae5a8da24700c4f6b
ben hengst <ben.hengst@gmail.com>
2011-04-21 11:39:49 -0700

    getting show to actually work

--- ghub.pl
+++ ghub.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use Data::Dumper;
+sub DUMP(@) { Dumper(@_) }
+sub D(@){ print DUMP(@_) }
 
 =head1 USEAGE
 
@@ -19,7 +22,7 @@ my $action = shift @ARGV;
 help() if $action eq 'help';
 
 die qq{$action is not a known action} unless $hub->can($action);
-print $hub->$action(@ARGV);
+D $hub->$action(@ARGV);
 
 
 #------------------------------------------------
@@ -34,40 +37,54 @@ sub D(@){ print DUMP(@_) }
 
 use Mouse;
 use JSON;
-require LWP::Simple; # we'll implement our own get
+require LWP::Simple; 
 
-has host => 
+use constant RW_STR => qw{is rw isa Str}; # PLEASE DO NOT EVER DO THIS, I'm lazy and this is a hack.
+
+has host     => RW_STR, default => 'http://github.com/api/v2/json';
+has username => RW_STR, predicate => 'has_username'; 
+has repos    => 
   is => 'rw',
-  isa => 'Str',
-  default => 'http://github.com/api/v2/json',
+  isa => 'HashRef',
+  lazy => 1,
+  default => sub{
+    my $self = shift;
+    #{ watched => $self->get(qw{repos watched}, $self->username)->{repositories},
+    #  show    => $self->get(qw{repos show   }, $self->username)->{repositories},
+    #}
+    my $out = {};
+    foreach my $repo ( @{ $self->get(qw{repos watched}, $self->username)->{repositories} } ,
+                       @{ $self->get(qw{repos show},    $self->username)->{repositories} }
+                     ) {
+      my $type = $repo->{fork}                       ? 'fork'
+               : $repo->{owner} ne $self->{username} ? 'watched'
+               :                                       'standard' ; 
+      push @{$out->{$type}}, $repo;
+      $out->{by_name}->{$repo->{name}}->{$repo->{url}} = $repo; # we want only unique URL's so build a hash
+    }
+    $out->{by_name} = { map{ $_ => [values %{$out->{by_name}->{$_}}] } keys %{$out->{by_name}} }; # because I don't care about the keys just the unique values, lets back out of the url as a key
+    $out;
+  }
 ;
 
-sub mkurl {
-  join '/', shift->host, @_;
-} 
-
-sub get {
+sub mkurl { join '/', shift->host, @_; } 
+sub get   { from_json( LWP::Simple::get( shift->mkurl(@_) ) ) } # can not use 'around' due to get being a function
+sub show  {
   my $self = shift;
-  my $data = from_json( LWP::Simple::get( $self->mkurl(@_) ) )
+  my $user = shift;
+  die 'A username is require to be passed to show' unless $user;
+  $self->username($user) unless $self->has_username;
+  if (@_) {
+    [ map{ my $R = $self->repos->{by_name}->{$_}; # this is going to be an arrayref (we deref below)
+           map{ $self->get(qw{repos show}, $_->{owner}, $_->{name})->{repository} } @$R
+         } @_
+    ];
+  }
+  else {
+    $self->repos;
+  }
 }
 
-sub repos {
-  my $self = shift;
-  my $view = shift;
-  return {repositories => [@{$self->repos(show=>@_)->{repositories}},@{$self->repos(watched=>@_)->{repositories}}]}
-      if $view eq 'all';
-  die qq{Unknown view type of $view, given to repos. Accepted values are 'show', 'watched', and 'all'.} unless $view =~ m/^(?:show|watched)$/;
-  shift->get('repos', $view,  @_);
-}
-
-sub show {
-  print join qq{\n} 
-      , map{ sprintf qq{%s %s %s}
-                   , $_->{fork} eq 'true' ? 'F' : ' '
-                   , $_->{url}
-                   , $_->{description}
-           } @{shift->repos( all => @_)->{repositories}}
-}
   
 
 }; # END
